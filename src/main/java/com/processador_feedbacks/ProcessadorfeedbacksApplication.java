2d311200effc1da415e55bb0fc1affb0067324cf
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-23 09:18:16 -0300

    commit inicial

---
+++ src/main/java/com/processador_feedbacks/ProcessadorfeedbacksApplication.java
@@ -0,0 +1,13 @@
+package com.processador_feedbacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessadorfeedbacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessadorfeedbacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/processador_feedbacks/ProcessadorfeedbacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processador_feedbacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorfeedbacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
